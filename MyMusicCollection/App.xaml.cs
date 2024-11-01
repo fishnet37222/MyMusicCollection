f17648aed66d9bee8be45809ba2b3faf514d063f
David Frischknecht <fishnet37222@gmail.com>
2024-11-01 12:34:10 -0400

    Setup app to use Prism and EF Core.

--- MyMusicCollection/App.xaml.cs
+++ MyMusicCollection/App.xaml.cs
@@ -2,8 +2,18 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Windows;
+
 namespace MyMusicCollection;
 
 public partial class App
 {
+	protected override void RegisterTypes(IContainerRegistry containerRegistry)
+	{
+	}
+
+	protected override Window CreateShell()
+	{
+		return Container.Resolve<MainWindow>();
+	}
 }
